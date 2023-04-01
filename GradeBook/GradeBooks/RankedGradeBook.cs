a7d323c421704ad22b3a305cdd7f9c57fe756537
Marcin Pawłowski <marcin.pawlowski1@microsoft.wsei.edu.pl>
2023-04-01 23:45:47 +0200

    Thirteenth test marked as completed after overriding CalculateStudentStatistics method.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -80,5 +80,16 @@ namespace GradeBook.GradeBooks
             if (Students.Count >= 5)
                 base.CalculateStatistics();
         }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+
+            if (Students.Count >= 5)
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
